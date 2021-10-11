76862b809686c4bf98c662446565416476afbfce
Daria <wwdarcool@yandex.ru>
2021-10-11 18:28:12 +0300

    add: adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const schemeSvg = document.querySelector('.scheme__svg'),
-currentPrice = document.querySelector('.price__counter');
+currentPrice = document.querySelector('.price__counter'),
+menuButton = document.querySelector('.nav__button'),
+menu = document.querySelector('.menu');
+
 let cost = 800,
     totalPrice = 0;
 
@@ -12,3 +15,7 @@ schemeSvg.addEventListener('click', (event) => {
         currentPrice.textContent = totalPrice;
     }
 });
+
+menuButton.addEventListener('click', (event) => {
+    menu.classList.toggle('is-open');
+})
